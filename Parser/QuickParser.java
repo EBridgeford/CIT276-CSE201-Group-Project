ea718f8fbe9f3409d3f53ba1d3339ac8038b1b3c
EBridgeford <none>
2014-11-03 23:04:38 -0500

    One line change
    Updated it so it will work with the latest csv files, didn't get a
    chance to update it to work with the lastest RecordOBJ tonight.

--- Parser/QuickParser.java
+++ Parser/QuickParser.java
@@ -29,7 +29,7 @@ private void fileToArray(LinkedList<String> tempList)
 
 try
 {
-	Scanner in = new Scanner(csvFile);
+ Scanner in = new Scanner(csvFile);
 
 
   
@@ -38,7 +38,7 @@ try
   {
     String tempString = in.nextLine();  
     
-    if (tempString.length() != 0)
+    if (tempString.length() > 8)
     {
       tempList.add(tempString);
     }
@@ -49,7 +49,7 @@ try
 
 catch (FileNotFoundException e)
 {
-System.out.println(e);
+  System.out.println(e);
 }
 }
 
@@ -57,57 +57,57 @@ System.out.println(e);
 private void parse(LinkedList<String> tempList)
 {
   int line = 0;
-    while (tempList.size() > line)
+  while (tempList.size() > line)
+  {
+    
+    String temp = csvFile.getName().substring(0,4); //puts the first four letters of the filename into a string for identification
+    
+    String[] tempArray = tempList.get(line).split(",");   
+    String strRFID = "", strCampus = "", strBldg = "", strLastScanedBy = "", strComments = "", strSerial = "", strHardware = "", timeStamp = "", strPurchaseOrder = "", strServiceTag ="";
+    int intRm = 0;
+    timeStamp = "14:13:12 09:20:20:20"; //fake date, replace with real date later 
+    
+    //identifies the kind of file passed in and parses it
+    if (temp.equals("inve") == true)
     {
-    
-    		String temp = csvFile.getName().substring(0,4); //puts the first four letters of the filename into a string for identification
-    		
-    			String[] tempArray = tempList.get(line).split(",");   
-       String strRFID = "", strCampus = "", strBldg = "", strLastScanedBy = "", strComments = "", strSerial = "", strHardware = "", timeStamp = "", strPurchaseOrder = "", strServiceTag ="";
-       int intRm = 0;
-       timeStamp = "14:13:12 09:20:20:20"; //fake date, replace with real date later 
-     
-       //identifies the kind of file passed in and parses it
-       if (temp.equals("inve") == true)
-       {
-       strRFID = tempArray[0];
-       strCampus = tempArray[1];
-       strBldg = tempArray[2];
-       intRm = Integer.parseInt(tempArray[3]);                              
-       strLastScanedBy = ""; //not available yet
-                      
-    
-         if (tempArray.length == 7)
-         {
-         strComments = tempArray[6];
-         }
-       }
-       
-       
-       if (temp.equals("init") == true)
-       {
-       strRFID = tempArray[0];
-       strSerial = tempArray[1];
-       strHardware = tempArray[2];                  
-       strLastScanedBy = ""; //not available yet?
-                      
-       
-
-         if (tempArray.length == 5)
-         {
-         strComments = tempArray[4];
-         }
-       
-       }
-     
+      strRFID = tempArray[0];
+      strCampus = tempArray[1];
+      strBldg = tempArray[2];
+      intRm = Integer.parseInt(tempArray[3]);                              
+      strLastScanedBy = ""; //not available yet
       
-    	   testList.add(new RecordObj(strRFID, strCampus, strBldg, intRm, strLastScanedBy, timeStamp, strComments, strPurchaseOrder, strServiceTag));                              
-
-       line++;
-        
+      
+      if (tempArray.length == 7)
+      {
+        strComments = tempArray[6];
+      }
     }
     
     
+    if (temp.equals("init") == true)
+    {
+      strRFID = tempArray[0];
+      strSerial = tempArray[1];
+      strHardware = tempArray[2];                  
+      strLastScanedBy = ""; //not available yet?
+      
+      
+      
+      if (tempArray.length == 5)
+      {
+        strComments = tempArray[4];
+      }
+      
+    }
+    
+    
+    testList.add(new RecordObj(strRFID, strCampus, strBldg, intRm, strLastScanedBy, timeStamp, strComments, strPurchaseOrder, strServiceTag));                              
+    
+    line++;
+    
+  }
+  
+  
   
   
 }
@@ -116,9 +116,9 @@ private void parse(LinkedList<String> tempList)
 public static void main (String [] args)
 {
 
-	 String str = "inventory.csv";
+  String str = "inventory.csv";
 
-		File csvFile = new File(str);
+  File csvFile = new File(str);
 
 
 
@@ -134,6 +134,6 @@ System.out.println(testList.get(i).getRm());
 System.out.println(testList.get(i).getComments());
 System.out.println();
 }
-	
+ 
 }
 }
