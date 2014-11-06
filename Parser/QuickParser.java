970766e098d54b68b1ea63a26f61821f5bb369fb
EBridgeford <none>
2014-11-06 00:04:01 -0500

    Returns ObservableList, works with latest RecordObj

--- Parser/QuickParser.java
+++ Parser/QuickParser.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
 
+import javafx.collections.*;
+
 public class QuickParser
 {
 
@@ -9,15 +11,17 @@ private ArrayList<RecordObj> testList = new ArrayList<RecordObj>();
 private File csvFile;
 
 
+
 //current returns ArrayList but can easily be converted to ObservableList
-public ArrayList Parse(File csvFile) 
+public ObservableList Parse(File csvFile) 
 {
   this.csvFile = csvFile;
   csv = new LinkedList<String>();
   fileToArray(csv);
   parse(csv);
-
-  return testList;
+  
+  ObservableList<RecordObj> observTestList = FXCollections.observableArrayList(testList);
+  return observTestList;
 
 }
 
@@ -63,46 +67,57 @@ private void parse(LinkedList<String> tempList)
     String temp = csvFile.getName().substring(0,4); //puts the first four letters of the filename into a string for identification
     
     String[] tempArray = tempList.get(line).split(",");   
-    String strRFID = "", strCampus = "", strBldg = "", strLastScanedBy = "", strComments = "", strSerial = "", strHardware = "", timeStamp = "", strPurchaseOrder = "", strServiceTag ="";
-    int intRm = 0;
-    timeStamp = "14:13:12 09:20:20:20"; //fake date, replace with real date later 
+    String strRFID = "", strLocation = "", strLastScanedBy = "", comments = "", strSerial = "", strPO = "", timeStamp = "", strPurchaseOrder = "", strServiceTag ="";
+
+    timeStamp = "14:13:12 09:20:20:20"; //fake date, update when we get a newer csv
+    strLastScanedBy = "nobody"; //fake person, update when we get a newer csv
+    
+    strRFID = tempArray[0]; //always the first
     
     //identifies the kind of file passed in and parses it
+   
     if (temp.equals("inve") == true)
     {
-      strRFID = tempArray[0];
-      strCampus = tempArray[1];
-      strBldg = tempArray[2];
-      intRm = Integer.parseInt(tempArray[3]);                              
-      strLastScanedBy = ""; //not available yet
       
-      
+      strLocation = tempArray[1] + " " + tempArray[2] + " " + tempArray[3];                           
+          
       if (tempArray.length == 7)
       {
-        strComments = tempArray[6];
+        comments = tempArray[6];
       }
     }
     
     
     if (temp.equals("init") == true)
     {
-      strRFID = tempArray[0];
+
       strSerial = tempArray[1];
-      strHardware = tempArray[2];                  
-      strLastScanedBy = ""; //not available yet?
-      
-      
-      
+      strPO = tempArray[2];                  
+        
       if (tempArray.length == 5)
       {
-        strComments = tempArray[4];
+        comments = tempArray[4];
       }
       
     }
     
+    RecordObj tempRecordObj = new RecordObj(strRFID,  strLastScanedBy, timeStamp, comments);
     
-    testList.add(new RecordObj(strRFID, strCampus, strBldg, intRm, strLastScanedBy, timeStamp, strComments, strPurchaseOrder, strServiceTag));                              
+    if (temp.equals("inve") == true)
+    {
+        tempRecordObj.setlocation(strLocation);
+    }
     
+    if (temp.equals("init") == true)
+    {
+
+    tempRecordObj.setServiceTag(strSerial);
+    tempRecordObj.setPurchaseOrder(strPO);
+      
+    }
+     
+    
+    testList.add(tempRecordObj);                                 
     line++;
     
   }
@@ -116,14 +131,17 @@ private void parse(LinkedList<String> tempList)
 public static void main (String [] args)
 {
 
-  String str = "inventory.csv";
+  String str = "initial.csv";
 
   File csvFile = new File(str);
 
 
 
 QuickParser test = new QuickParser();
-ArrayList<RecordObj> testList = test.Parse(csvFile);
+ObservableList<RecordObj> testList = test.Parse(csvFile);
+
+if (str.equals("inventory.csv"))
+		{
 
 for (int i = 0; testList.size() > i; i++)
 {
@@ -131,9 +149,31 @@ System.out.println(testList.get(i).getRfid());
 System.out.println(testList.get(i).getCampus());
 System.out.println(testList.get(i).getBldg());
 System.out.println(testList.get(i).getRm());
+System.out.println(testList.get(i).getScanMonth()+"/"+testList.get(i).getScanDay()+"/"+testList.get(i).getScanYear());
+System.out.println(testList.get(i).getLastScanedBy());
 System.out.println(testList.get(i).getComments());
 System.out.println();
 }
- 
+		}
+
+if (str.equals("initial.csv"))
+{
+
+for (int i = 0; testList.size() > i; i++)
+{
+System.out.println(testList.get(i).getRfid());
+System.out.println(testList.get(i).getServiceTag());
+System.out.println(testList.get(i).getPurchaseOrder());
+System.out.println(testList.get(i).getLastScanedBy());
+System.out.println(testList.get(i).getScanMonth()+"/"+testList.get(i).getScanDay()+"/"+testList.get(i).getScanYear());
+System.out.println(testList.get(i).getComments());
+System.out.println();
 }
+}
+ 
+
+
+
+}
+
 }
