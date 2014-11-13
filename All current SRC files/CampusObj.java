ca767e7fb122e2816ea989143d58f25518c2338c
hadleyps <hadleyps@miamioh.edu>
2014-11-13 02:28:27 -0500

    added files
    added a location with all source files to make digging through folders
    easier
    added the newest Project flowchart with updated info
    added the source file for the flowchart if anyone wants to download
    NClass to edit it, the application is on sourceforge

---
+++ All current SRC files/CampusObj.java
@@ -0,0 +1,70 @@
+/*
+ * *******************************************************************************************************
+ * ********  **           ***      ******   ******** **           ***      ***     *** ******** ***    *** 
+ * **    *** **          **  *     **   **  ***      **          **  *      ***   ***  ***      ****   ***
+ * ********  **         **    *    **    ** ******** **         **    *      *** ***   ******** ** **  *** 
+ * ********  **        *********   **    ** ******** **        *********      *****    ******** **  ** *** 
+ * **    *** *******  ***********  **   **  ***      *******  ***********      ***     ***      **   ***** 
+ * ********  ******* **         ** ******   ******** ******* **         **      *      ******** **    **** 
+ * *******************************************************************************************************
+ * *********************************************** BLADELAVEN STUDIOS ************************************
+
+ */
+package devicetrackerpro;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * This object holds the value of the campus
+ * @author Patrick Scott Hadley
+ */
+public class CampusObj{
+   
+    private StringProperty strCampus;
+    
+    /**
+     * This method Instantiates the campus object
+     * @param campus 
+     */
+    public CampusObj(String campus){
+        this.strCampus = new SimpleStringProperty(campus);                  
+    }//END INITIALIZATION
+    
+     /**
+      * This method returns the string value of the campus object.
+      * @return String Campus
+      */
+     public final String getCampus(){return this.strCampus.get();}      
+             
+     /**
+      * This method sets the campus properties string value.
+      * @param string
+      */
+     public final void setCampus(String str){this.strCampus.set(str);}  
+     
+     /**
+      * This method returns the campus object's property value
+      * @return simpleStringProperty
+      */
+     public StringProperty campusProperty(){return this.strCampus;}         
+     
+     /**
+      * This method is used for testing.
+      * @param args 
+      */
+     public static void main(String[] args) {
+        
+        //Initialization test
+        CampusObj room = new CampusObj("HAM");
+        
+        //Test getters
+        System.out.print("Location : "+room.getCampus()+"\n");
+        
+        //Test setters
+        room.setCampus("MUM");
+        
+        //Test getters again
+        System.out.print("Location : "+room.getCampus()+"\n");
+        
+    }//END MAIN
+}//END CLASS
